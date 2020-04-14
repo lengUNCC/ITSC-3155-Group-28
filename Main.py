1d39ac44d88d3e1539063503f7a7d9f4bfc3750d
lengUNCC <59849728+lengUNCC@users.noreply.github.com>
2020-04-14 00:02:54 -0400

    Add files via upload
    Update to account for tax

--- Main.py
+++ Main.py
@@ -5,16 +5,17 @@ total_amount = 0
 repeat = ''
 while True:
     product = input('What is your product : ')
-    unit_price = Invoice().inputNumber("Please enter unit price : ")
+    unit_price = Invoice().inputNumber("Please enter unit price : $")
     qnt = Invoice().inputNumber("Please enter quantity of product : ")
     discount = Invoice().inputNumber("Discount percent (%) : ")
     markup = Invoice().inputNumber("Markup percent (%) : ")                 #test line
+    tax = Invoice().inputNumber("Tax percent (%) : ")
     repeat = Invoice().inputAnswer("Another product? (y,n) : ")
-    result = Invoice().addProduct(qnt, unit_price, discount, markup)        #modified lines with updated parameters
+    result = Invoice().addProduct(qnt, unit_price, discount, markup, tax)        #modified lines with updated parameters
     products[product] = result
     if repeat == "n":
         break
 
 total_amount = Invoice().totalPurePrice(products)
 
-print("Your total pure price is: ", total_amount)
+print("Your total pure price is: $", total_amount)
