7636ee633033528d364f86aa9ba538b0b4ad300c
JitalSoni <60207640+JitalSoni@users.noreply.github.com>
2020-04-19 00:33:17 -0400

    Add files via upload

--- Main.py
+++ Main.py
@@ -4,18 +4,36 @@ products = {}
 total_amount = 0
 repeat = ''
 while True:
-    product = input('What is your product : ')
-    unit_price = Invoice().inputNumber("Please enter unit price : $")
+    product = Invoice().inputProduct('What is your product : ')
+    unit_price = Invoice().inputNumber("Please enter unit price : ")
     qnt = Invoice().inputNumber("Please enter quantity of product : ")
     discount = Invoice().inputNumber("Discount percent (%) : ")
     markup = Invoice().inputNumber("Markup percent (%) : ")                 #test line
     tax = Invoice().inputNumber("Tax percent (%) : ")
     repeat = Invoice().inputAnswer("Another product? (y,n) : ")
+
     result = Invoice().addProduct(qnt, unit_price, discount, markup, tax)        #modified lines with updated parameters
     products[product] = result
     if repeat == "n":
         break
+items_in_cart = Invoice().inputProduct(products)
+
+while True:
+    checkout = Invoice().inputAnswer("This is your final cart. Are you sure you want to checkout? (y,n)")
+    if checkout == 'n':
+        break
+
+while True:
+    update = Invoice().inputAnswer("Do you want to update the quantity? (y,n) : ")
+    qntUpdate = Invoice().inputNumber("Please enter quantity of product : ")
+    if update == 'n':
+        break
 
 total_amount = Invoice().totalPurePrice(products)
+total_count = Invoice().totalCount(products)
+result1 = Invoice().inputAnswer(qntUpdate)
+update_1 = Invoice().updateCart(products, qntUpdate)
+print("Your total pure price is: ", total_amount)
+print('Updated quantity: ', update_1)
 
-print("Your total pure price is: $", total_amount)
+
